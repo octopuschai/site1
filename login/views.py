ed6e8a6299e78e976b877a9cf5b6924087813a73
Jones <chaiy2004@163.com>
2019-04-04 17:05:57 +0800

    use django forms

--- login/views.py
+++ login/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from . import models, forms
 
 # Create your views here.
 def index(request):
@@ -7,11 +8,22 @@ def index(request):
 
 def login(request):
     if request.method == 'POST':
-        username = request.POST.get('username')
-        password = request.POST.get('password')
-        print(username, password)
-        return redirect('/index/')
-    return render(request, 'login/login.html')
+        login_form = forms.UserForm(request.POST)
+        message = '请检查填写的内容！'
+        if login_form.is_valid():
+            username = login_form.cleaned_data['username']
+            password = login_form.cleaned_data['password']
+            try:
+                user = models.User.objects.get(name=username)
+                if user.password == password:
+                    return redirect('/index/')
+                else:
+                    message = '密码不正确'
+            except:
+                message = '用户名不存在'
+        return render(request, 'login/login.html', {'message':message, 'login_form':login_form})
+    login_form = forms.UserForm()
+    return render(request, 'login/login.html', {'login_form':login_form})
 
 def register(request):
     pass
