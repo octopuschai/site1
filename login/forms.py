989c0734087acf7dd5e6273f72aac9f7a21518d2
Jones <chaiy2004@163.com>
2019-04-09 16:46:25 +0800

    update html template and add register() content in views.py

--- login/forms.py
+++ login/forms.py
@@ -1,8 +1,14 @@
 from django import forms
+
+# 导入验证码库
 from captcha.fields import CaptchaField
-from . import models
+
+# from . import models
 
 class UserForm(forms.Form):
+    '''
+    创建用户登陆表单类
+    '''
     username = forms.CharField(label='用户名', max_length=128, widget=forms.TextInput(attrs={'class':'form-control','placeholder':'username'}))
     password = forms.CharField(label='密码', max_length=256, widget=forms.PasswordInput(attrs={'class':'form-control','placeholder':'password'}))
     captcha = CaptchaField(label='验证码')
@@ -19,3 +25,19 @@ class UserForm(forms.ModelForm):
         self.fields['name'].label = '用户名'
         self.fields['password'].label = '密码'
 '''
+
+class RegisterForm(forms.Form):
+    '''
+    创建用户注册表单类
+    '''
+    gender = (
+        ('male', '男'),
+        ('female', '女'),
+    )
+
+    username = forms.CharField(label='用户名', max_length=128, widget=forms.TextInput(attrs={'class':'form-control'}))
+    password = forms.CharField(label='密码', max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
+    confirm_password = forms.CharField(label='确认密码', max_length=256, widget=forms.TextInput(attrs={'class':'form-control'}))
+    email = forms.EmailField(label='邮箱', widget=forms.EmailInput(attrs={'class':'form-control'}))
+    sex = forms.ChoiceField(label='性别', choices=gender)
+    captcha = CaptchaField(label='验证码')
--- login/views.py
+++ login/views.py
@@ -7,6 +7,11 @@ def index(request):
     return render(request, 'login/index.html')
 
 def login(request):
+    '''
+    登陆视图函数
+    :param request:
+    :return:
+    '''
     if request.session.get('is_login', None):
         return redirect('/index/')
     if request.method == 'POST':
@@ -31,10 +36,52 @@ def login(request):
     return render(request, 'login/login.html', {'login_form':login_form})
 
 def register(request):
-    pass
-    return render(request, 'login/register.html')
+    '''
+    注册视图函数
+    :param request:
+    :return:
+    '''
+    if request.session.get('is_login', None):
+        return redirect('/index/')
+    if request.method == 'POST':
+        register_form = forms.RegisterForm(request.POST)
+        message = '请检查填写的内容！'
+        if register_form.is_valid():
+            username = register_form.cleaned_data['username']
+            password = register_form.cleaned_data['password']
+            confirm_password = register_form.cleaned_data['confirm_password']
+            email = register_form.cleaned_data['email']
+            sex = register_form.cleaned_data['sex']
+
+            # 在数据库中取输入的用户名和邮箱，判断是否已存在
+            same_user = models.User.objects.filter(name=username)
+            same_email = models.User.objects.filter(email=email)
+            if same_user:
+                message = '该用户名已注册，请换另一个用户名进行注册'
+            elif password != confirm_password:
+                message = '第2次输入的确认密码不正确，请重新输入密码'
+            elif same_email:
+                message = '该邮箱已注册，请换另一个邮箱进行注册'
+            else:
+                new_user = models.User()
+                new_user.name = username
+                new_user.password = password
+                new_user.email = email
+                new_user.sex = sex
+                new_user.save()
+                return redirect('/login/')
+
+        return render(request, 'login/register.html', {'message':message, 'register_form':register_form})
+
+    register_form = forms.RegisterForm()
+    return render(request, 'login/register.html', {'register_form':register_form})
 
 def logout(request):
+    '''
+    登出视图函数
+    :param request:
+    :return:
+    '''
     if request.session.get('is_login', None):
         request.session.flush()
     return redirect('/index/')
